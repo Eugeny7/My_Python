445f1d6ca09259a6fdf49e9937b791a6c97ad652
Eugeny Puchkov <pu4ckov.eugeny@yandex.ru>
2025-02-11 23:33:08 +0300

    Otus

--- tests_otus.py
+++ tests_otus.py
@@ -1,6 +1,6 @@
 from triangle import Triangle
 
-
 def triangle():
-    my_triangle = Triangle(10, 20, 15)
-    print (my_triangle.area())
+    my_triangle = Triangle(13, 14, 15)
+    return my_triangle.area()
+print(triangle())
--- triangle.py
+++ triangle.py
@@ -3,19 +3,15 @@ import  math
 class Triangle():
     def __init__(self, side_a, side_b, side_c):
         if side_a <= 0 or side_b <= 0 or side_c <=0:
-            raise ValueError(f'Creating a figure: '"Triangle"' impossible')
+            raise ValueError(f'Creating a figure: "triangle" impossible')
         self.side_a = side_a
         self.side_b = side_b
         self.side_c = side_c
         self.name = "Triangle"
 
     def area(self):
-        p = self.side_a + self.side_b + self.side_c / 2
-        return math.sqrt(p(p - self.side_a)(p - self.side_b)(p - self.side_c))
+        p = (self.side_a + self.side_b + self.side_c)/ 2
+        return math.sqrt(p*(p - self.side_a)*(p - self.side_b)*(p - self.side_c))
 
-    def perimiter(self):
+    def perimeter(self):
         return self.side_a + self.side_b +self.side_c
-def triangle():
-    my_triangle = Triangle(10, 20, 20)
-    return my_triangle.area()
-triangle()
