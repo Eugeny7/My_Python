c6a0252edeb53d26b2519f33191eae5f76de6a69
Eugeny Puchkov <pu4ckov.eugeny@yandex.ru>
2025-02-12 21:58:15 +0300

    square,rectangle

---
+++ src/rectangle.py
@@ -0,0 +1,17 @@
+import math
+
+class Rectangle():
+    def __init__(self, side_a, side_b):
+        if side_a <=0 or side_b <=0:
+            raise ValueError('The creation of the figure "rectangle" is impossible')
+        self.side_a = side_a
+        self.side_b = side_b
+        self.name = 'Rectangle'
+
+    def area(self):
+        return self.side_a * self.side_b
+
+    def perimeter(self):
+        return (self.side_a + self.side_b) * 2
+
+
--- test_home_otus/tests_otus.py
+++ test_home_otus/tests_otus.py
@@ -1,7 +1,13 @@
 from src.triangle import *
 from src.сircle import *
+from src.square import *
+from src.rectangle import *
 
-
+# def triangle_perimeter():
+#     my_triangle = Triangle(13, 14 , 15)
+#     return my_triangle.perimeter()
+# print(triangle_perimeter())
+#
 # def triangle_area():
 #     my_triangle = Triangle(13, 14, 15)
 #     return my_triangle.area()
@@ -11,11 +17,30 @@ from src.сircle import *
 #     my_circle = Circle(20)
 #     return my_circle.area()
 # print(circle_area())
+#
+# def circle_perimeter():
+#     my_circle = Circle(20)
+#     return my_circle.perimeter()
+# print(circle_perimeter())
+#
+# def square_area():
+#     my_square = Square(10)
+#     return my_square.area()
+# print(square_area())
 
-def circle_perimiter():
-    my_circle = Circle(20)
-    return my_circle.perimeter()
-print(circle_perimiter())
+# def rectangle_area():
+#     my_rectangle = Rectangle(5, 10)
+#     return my_rectangle.area()
+# print(rectangle_area())
+#
+# def rectangle_perimeter():
+#     my_rectangle = Rectangle(5, 10)
+#     return my_rectangle.perimeter()
+# print(rectangle_perimeter())
 
 
+# def square_perimeter():
+#     my_square = Square(5)
+#     return my_square.perimeter()
+# print(square_perimeter())
 
