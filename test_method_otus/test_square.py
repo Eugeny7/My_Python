723bdf430b7600a25dbb0b99c67ebb40b40b1db6
Eugeny Puchkov <pu4ckov.eugeny@yandex.ru>
2025-02-13 23:12:53 +0300

    done home_work

---
+++ test_method_otus/test_square.py
@@ -0,0 +1,40 @@
+from src.сircle import *
+from src.square import *
+from src.rectangle import *
+from src.triangle import *
+import pytest
+
+
+@pytest.mark.parametrize('side, area, perimeter, name',
+                         [(10, 100, 40, 'Square')])
+def test_square_ar_per_name(side, area, perimeter, name):
+    square = Square(side)
+    assert square.name == name
+    assert square.area() == area
+    assert square.perimeter() == perimeter
+
+
+@pytest.mark.parametrize('radius, side_a, side_b, side_c',
+                         [(10, 20, 30, 15)])
+def test_square_add_area(radius, side_a, side_b, side_c):
+    circle = Circle(radius)
+    triangle = Triangle(side_a, side_b, side_c)
+    square = Square(side_a)
+    rectangle = Rectangle(side_a, side_b)
+    assert square.add_area(circle) == square.area() + circle.area()
+    assert square.add_area(triangle) == square.area() + triangle.area()
+    assert square.add_area(rectangle) == square.area() + rectangle.area()
+
+
+@pytest.mark.parametrize('radius, side_a, side_b, side_c',
+                         [(10, 20, 30, -15),
+                          (0, -1, 21, 7)])
+def test_square_add_area_negative(radius, side_a, side_b, side_c):
+    with pytest.raises(ValueError):
+        circle = Circle(radius)
+        triangle = Triangle(side_a, side_b, side_c)
+        square = Square(side_a)
+        rectangle = Rectangle(side_a, side_b)
+        assert square.add_area(circle) == square.area() + circle.area()
+        assert square.add_area(triangle) == square.area() + triangle.area()
+        assert square.add_area(rectangle) == square.area() + rectangle.area()
--- test_method_otus/test_triangle.py
+++ test_method_otus/test_triangle.py
@@ -25,3 +25,17 @@ def test_triangle_add_area(radius, side_a, side_b, side_c):
     assert triangle.add_area(circle) == triangle.area() + circle.area()
     assert triangle.add_area(square) == triangle.area() + square.area()
     assert triangle.add_area(rectangle) == triangle.area() + rectangle.area()
+
+
+@pytest.mark.parametrize('radius, side_a, side_b, side_c',
+                         [(-11, 32, 23, 12),
+                          (0, 83, 77, 111)])
+def test_triangle_add_area(radius, side_a, side_b, side_c):
+    with pytest.raises(ValueError):
+        circle = Circle(radius)
+        triangle = Triangle(side_a, side_b, side_c)
+        square = Square(side_a)
+        rectangle = Rectangle(side_a, side_b)
+        assert triangle.add_area(circle) == triangle.area() + circle.area()
+        assert triangle.add_area(square) == triangle.area() + square.area()
+        assert triangle.add_area(rectangle) == triangle.area() + rectangle.area()
