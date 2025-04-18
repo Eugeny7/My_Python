d18d0be4d7efb1212dab9b83a79c5fa44f5b7073
Eugeny Puchkov <pu4ckov.eugeny@yandex.ru>
2025-04-19 01:11:58 +0300

    Test_Currency

--- Otus/hw_4/locators.py
+++ Otus/hw_4/locators.py
@@ -4,25 +4,34 @@ class MainPage:
     INPUT_SEARCH = ('xpath', '//input[@name="search"]')
     BUTTON_SHOP = ('xpath', '//button[@type="button" and @data-bs-toggle="dropdown"]')
     BUTTON_CHECKOUT = ('xpath', '//span[@class="d-none d-md-inline" and text()="Checkout"]')
+    ITEM_NAME = ('xpath', '//h4/a[text()="MacBook"]')
+    ADD_CART_BUTTON = ('css selector', 'button[formaction*="cart.add"]')
+    SHOPPING_CART_BUTTON = ('xpath', '//span[@class="d-none d-md-inline" and text()="Shopping Cart"]')
+    CURRENCY_DROPDOWN_MAIN = ('xpath', '//span[text()="Currency"]')
+    CURRENCY_EURO_BUTTON = ('xpath', '//a[@class="dropdown-item" and contains(text(), "Euro")]')
+    CURRENCY_Sterling_BUTTON = ('xpath', '//a[@class="dropdown-item" and contains(text(), "Sterling")]')
+    CURRENCY_Dollar_BUTTON = ('xpath', '//a[@class="dropdown-item" and contains(text(), "Dollar")]')
+    CURRENCY_STRONG = ('xpath', '//strong')
 
 
 class AdminPage:
+    OPENCART_LOGIN = 'user'
+    OPENCART_PASSWORD = 'bitnami'
+    DEFAULT_EMAIL_USER = 'user@example.com'
+
     INPUT_USERNAME = ('xpath', '//input[@id="input-username"]')
     INPUT_PASSWORD = ('xpath', '//input[@id="input-password"]')
     BUTTON_SUBMIT = ('xpath', '//button[@type="submit"]')
     LINK_OPENCART = ('xpath', '//a[text()="OpenCart"]')
     HEADER_FORM = ('xpath', '//a[text()="OpenCart"]')
+    DROPDOWN_MENU = ('xpath', '//span[@class="d-none d-md-inline d-lg-inline"]')
+    PROFILE_BUTTON = ('xpath', '//a[@class="dropdown-item" and contains(@href, "user_token")]')
+    INPUT_EMAIL = ('xpath', '//input[@id="input-email"]')
 
 
 class RegistrationPage:
-
     INPUT_FIRST_NAME = ('xpath', '//input[@id="input-firstname"]')
-    INPUT_LAST_NAME = ('xpath', '//input[@id="input-lastname"]' )
-    INPUT_EMAIL = ('xpath', '//input[@id="input-email"]' )
-    INPUT_PASSWORD = ('xpath', '//input[@id="input-password"]' )
-    BUTTON_CONTINUE = ('xpath', '//button[@type="submit" and text()="Continue"]' )
-
-# Other
-
-ITEM_CART = ('xpath', '//button[@type="button" and @data-bs-toggle ="dropdown"]')
-# wait.until(EC.visibility_of_element_located(ITEM_CART)).text
+    INPUT_LAST_NAME = ('xpath', '//input[@id="input-lastname"]')
+    INPUT_EMAIL = ('xpath', '//input[@id="input-email"]')
+    INPUT_PASSWORD = ('xpath', '//input[@id="input-password"]')
+    BUTTON_CONTINUE = ('xpath', '//button[@type="submit" and text()="Continue"]')
--- Otus/hw_4/test_first.py
+++ Otus/hw_4/test_first.py
@@ -1,7 +1,9 @@
 import pytest
+from selenium.webdriver import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from Otus.hw_4.locators import *
+
 
 @pytest.mark.smoke
 def test_main_page(browser, url):
@@ -13,6 +15,7 @@ def test_main_page(browser, url):
     wait.until(EC.visibility_of_element_located(MainPage.BUTTON_SHOP)).click()
     wait.until(EC.visibility_of_element_located(MainPage.BUTTON_CHECKOUT)).click()
 
+
 def test_admin_page(browser, url):
     wait = WebDriverWait(browser, 3)
     browser.get(f'{url}/administration')
@@ -22,6 +25,7 @@ def test_admin_page(browser, url):
     wait.until(EC.visibility_of_element_located(AdminPage.BUTTON_SUBMIT)).click()
     wait.until(EC.visibility_of_element_located(AdminPage.LINK_OPENCART)).click()
 
+
 def test_registration_page(browser, url):
     wait = WebDriverWait(browser, 4)
     browser.get(f'{url}/en-gb?route=account/register')
@@ -30,3 +34,49 @@ def test_registration_page(browser, url):
     wait.until(EC.visibility_of_element_located(RegistrationPage.INPUT_EMAIL)).click()
     wait.until(EC.visibility_of_element_located(RegistrationPage.INPUT_PASSWORD)).click()
     wait.until(EC.visibility_of_element_located(RegistrationPage.BUTTON_CONTINUE)).click()
+
+
+def test_admin_login(browser, url):
+    wait = WebDriverWait(browser, 5)
+    actions = ActionChains(browser)
+    browser.get(f'{url}/administration/')
+    input_name = wait.until(EC.visibility_of_element_located(AdminPage.INPUT_USERNAME))
+    input_password = wait.until(EC.visibility_of_element_located(AdminPage.INPUT_PASSWORD))
+    actions.move_to_element(input_name).click().send_keys(AdminPage.OPENCART_LOGIN).perform()
+    actions.move_to_element(input_password).click().send_keys(AdminPage.OPENCART_PASSWORD).perform()
+    wait.until(EC.visibility_of_element_located(AdminPage.BUTTON_SUBMIT)).click()
+    wait.until(EC.visibility_of_element_located(AdminPage.DROPDOWN_MENU)).click()
+    wait.until(EC.visibility_of_element_located(AdminPage.PROFILE_BUTTON)).click()
+    user_name = wait.until(EC.visibility_of_element_located(AdminPage.INPUT_USERNAME)).get_attribute('value')
+    user_email = wait.until(EC.visibility_of_element_located(AdminPage.INPUT_EMAIL)).get_attribute('value')
+    assert user_name == AdminPage.OPENCART_LOGIN and user_email == AdminPage.DEFAULT_EMAIL_USER
+
+
+def test_shopping_cart(browser, url):
+    wait = WebDriverWait(browser, 7)
+    actions = ActionChains(browser)
+    browser.get(url)
+    name_item_main = wait.until(EC.visibility_of_element_located(MainPage.ITEM_NAME)).text
+    item = wait.until(EC.visibility_of_element_located(MainPage.ADD_CART_BUTTON))
+    actions.pause(0.5).move_to_element(item).click().perform()
+    shopping_cart = wait.until(EC.visibility_of_element_located(MainPage.SHOPPING_CART_BUTTON))
+    actions.move_to_element(shopping_cart).pause(0.2).click().perform()
+    name_item_shopping = wait.until(EC.visibility_of_element_located(MainPage.ITEM_NAME)).text
+    assert name_item_main == name_item_shopping, f'The goods are not consistent'
+
+
+@pytest.mark.parametrize('currency',
+                         [MainPage.CURRENCY_EURO_BUTTON,
+                          MainPage.CURRENCY_Dollar_BUTTON,
+                          MainPage.CURRENCY_Sterling_BUTTON])
+def test_currency(browser, url, currency):
+    wait = WebDriverWait(browser, 5)
+    actions = ActionChains(browser)
+    browser.get(url)
+    element = wait.until(EC.visibility_of_element_located(MainPage.CURRENCY_DROPDOWN_MAIN))
+    actions.move_to_element(element).click().perform()
+    item_currency = wait.until(EC.visibility_of_element_located(currency))
+    symbol_currency = item_currency.text[0]
+    actions.move_to_element(item_currency).click().perform()
+    currency_strong = wait.until(EC.visibility_of_element_located(MainPage.CURRENCY_STRONG)).text
+    assert symbol_currency == currency_strong
