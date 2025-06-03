1f26a3ac18acec0ea4865d7cc8498db95579d218
Eugeny Puchkov <pu4ckov.eugeny@yandex.ru>
2025-06-03 23:13:16 +0300

    page

---
+++ selenium/stepik/Page/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        self.open()
+        assert LoginPageLocators.TEXT in self.browser.current_url
+
+    def should_be_login_form(self):
+        self.open()
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM) , 'Login form not found'
+
+    def should_be_register_form(self):
+        self.open()
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), 'Registration form not found'
---
+++ selenium/stepik/test_main_page.py
@@ -0,0 +1,17 @@
+from .Page.main_page import MainPage
+from .Page.login_page import LoginPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, "http://selenium1py.pythonanywhere.com/")
+    page.open()
+    page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, "http://selenium1py.pythonanywhere.com/")
+    page.open()
+    page.should_be_login_link()
+
+def test_login_and_registration_form(browser):
+    page = LoginPage(browser, 'https://selenium1py.pythonanywhere.com/ru/accounts/login/')
+    page.should_be_login_page()
