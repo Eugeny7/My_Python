d2b33610c91b9ee31ba007738f088298616c0869
Eugeny Puchkov <pu4ckov.eugeny@yandex.ru>
2025-06-17 22:45:00 +0300

    options --language

--- stepik_project/conftest.py
+++ stepik_project/conftest.py
@@ -1,14 +1,38 @@
-import pytest
-from selenium import webdriver
 import time
 import math
+from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture(scope='function')
-def browser():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    yield driver
-    driver.quit()
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or safari")
+    parser.addoption('--language', action ='store', default='ru')
+
+@pytest.fixture
+def browser_name(request):
+    return request.config.getoption('browser_name')
+@pytest.fixture
+def browser_language(request):
+    return request.config.getoption('language')
+
+@pytest.fixture(scope="function")
+def browser(browser_name, browser_language):
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+    if browser_name == 'chrome':
+        browser = webdriver.Chrome(options=options)
+        browser.maximize_window()
+    elif browser_name == 'safari':
+        browser = webdriver.Safari(options=options)
+        browser.maximize_window()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or safari")
+    yield browser
+    browser.quit()
+
+
 
 @pytest.fixture(scope='function')
 def math_result():
---
+++ stepik_project/test_items.py
@@ -0,0 +1,11 @@
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+def test_should_be_basket_btn(browser):
+    browser.get(link)
+    try:
+        element = browser.find_element(By.CSS_SELECTOR, '.btn.btn-primary.btn')
+    except NoSuchElementException:
+        element = None
+    assert element is not None, 'Not found element'
