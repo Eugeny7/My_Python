7c6ec1f1dc5f6cca3d720841bd5eb94213dc4b3a
Eugeny Puchkov <pu4ckov.eugeny@yandex.ru>
2025-07-27 11:56:50 +0300

    appium refactor

--- Otus/Appium/Android/test_android.py
+++
@@ -1,5 +0,0 @@
-from appium.webdriver.common.appiumby import AppiumBy
-
-def test_gmail(driver):
-    element = driver.find_element(AppiumBy.XPATH, '//android.widget.TextView[@resource-id="com.google.android.gm:id/welcome_tour_got_it"]')
-    element.click()
